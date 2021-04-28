eb54a0fdf0e50df9603994965aee4fd09719f399
Naim Zaman <egnaimur@gmail.com>
2021-04-28 23:03:05 +0600

    Weather App Static

---
+++ js/active.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($){
+
+	jQuery('.owl-carousel').owlCarousel({
+		items:3,
+		loop:true,
+		autoplay:true,
+		center:true,
+		nav:true,
+		navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+	});
+
+});
